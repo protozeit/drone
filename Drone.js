42d6dfd7b27227b5917671d197e9e054c74d5bc3
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-08-27 18:04:23 +0100

    Add help message.

--- Drone.js
+++ Drone.js
@@ -1,70 +1,75 @@
 function Drone() {
-	this.position = createVector(windowWidth / 2, windowHeight / 2);
-  	this.velocity = createVector(0.1, 0);
-  	this.acceleration = createVector(0, 0);
-  	// sprite size
-  	this.scale = 0.1;
-  	// air resitance
-  	this.c = 0.02;
-  	// movement speed
-  	this.speed = 3;
-	
+  this.position = createVector(windowWidth / 2, windowHeight / 2);
+  this.velocity = createVector(0.1, 0);
+  this.acceleration = createVector(0, 0);
+  // sprite size
+  this.scale = 0.1;
+  // air resitance
+  this.c = 0.02;
+  // movement speed
+  this.speed = 3;
 
-	this.update = () => {
-		// Player input
-		this.acceleration.x = (right - left) * (turbo+1) * this.speed;
-		this.acceleration.y = (down - up) * (turbo+1) * this.speed;
+  this.update = () => {
+    // Player input
+    this.acceleration.x = (right - left) * (turbo + 1) * this.speed;
+    this.acceleration.y = (down - up) * (turbo + 1) * this.speed;
 
-		// Add air resistance
-		let v = this.velocity.mag();
-		let dragMagnitude = this.c * v;
-		let dragForce = this.velocity.copy();
-		dragForce.mult(-1);
-		dragForce.mult(dragMagnitude);
+    // Add air resistance
+    let v = this.velocity.mag();
+    let dragMagnitude = this.c * v;
+    let dragForce = this.velocity.copy();
+    dragForce.mult(-1);
+    dragForce.mult(dragMagnitude);
 
-		this.acceleration.add(dragForce);
+    this.acceleration.add(dragForce);
 
-		// Collision detection
-		if (this.position.x < 0) { 
-			this.position.x = 0;
-			this.velocity.x = -this.velocity.x;
-			left = 0;
-			right = 0;
-			turbo = 0;
-		}
-		if (this.position.x > windowWidth - drone_sprite.width*this.scale) {
-			this.position.x = windowWidth - drone_sprite.width*this.scale;
-			this.velocity.x = -this.velocity.x;
-			left = 0;
-			right = 0;
-			turbo = 0;
-		}
-		if (this.position.y < 0) { 
-			this.position.y = 0;
-			this.velocity.y = -this.velocity.y;
-			up = 0;
-			down = 0;
-			turbo = 0;
-		}
-		if (this.position.y > windowHeight - drone_sprite.height*this.scale) {
-			this.position.y = windowHeight - drone_sprite.height*this.scale;
-			this.velocity.y = -this.velocity.y;
-			up = 0;
-			down = 0;
-			turbo = 0
-		}
+    // Collision detection
+    if (this.position.x < 0) {
+      this.position.x = 0;
+      this.velocity.x = -this.velocity.x;
+      left = 0;
+      right = 0;
+      turbo = 0;
+    }
+    if (this.position.x > windowWidth - drone_sprite.width * this.scale) {
+      this.position.x = windowWidth - drone_sprite.width * this.scale;
+      this.velocity.x = -this.velocity.x;
+      left = 0;
+      right = 0;
+      turbo = 0;
+    }
+    if (this.position.y < 0) {
+      this.position.y = 0;
+      this.velocity.y = -this.velocity.y;
+      up = 0;
+      down = 0;
+      turbo = 0;
+    }
+    if (this.position.y > windowHeight - drone_sprite.height * this.scale) {
+      this.position.y = windowHeight - drone_sprite.height * this.scale;
+      this.velocity.y = -this.velocity.y;
+      up = 0;
+      down = 0;
+      turbo = 0;
+    }
 
-		// propagate acceleration to movement
-		this.velocity.add(this.acceleration);
-		this.position.add(this.velocity);
-		
-		// speed limit
-		this.velocity.limit(12);
+    // propagate acceleration to movement
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
 
-		this.acceleration.mult(0);
-	}
+    // speed limit
+    this.velocity.limit(12);
 
-	this.show = () => {
-		image(drone_sprite, this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale);
-	}
+    this.acceleration.mult(0);
+  };
+
+  this.show = () => {
+    image(
+      drone_sprite,
+      this.position.x,
+      this.position.y,
+      drone_sprite.width * this.scale,
+      drone_sprite.height * this.scale
+    );
+  };
 }
--- sketch.js
+++ sketch.js
@@ -12,27 +12,32 @@ let up = 0;
 let down = 0;
 let turbo = 0;
 
-function preload(){
-  drone_sprite = loadImage('sprites/colored_drone.png');
-  guy_sprite1 = loadImage('sprites/TheGuy.png');
-  guy_sprite2 = loadImage('sprites/hands_up.png');
-  guy_sprite3 = loadImage('sprites/head_down.png');
-  logo = loadImage('sprites/logo-pixelated.png');
+function preload() {
+  drone_sprite = loadImage("sprites/colored_drone.png");
+  guy_sprite1 = loadImage("sprites/TheGuy.png");
+  guy_sprite2 = loadImage("sprites/hands_up.png");
+  guy_sprite3 = loadImage("sprites/head_down.png");
+  logo = loadImage("sprites/logo-pixelated.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   d = new Drone();
-  guy = new Animation(...[guy_sprite1, guy_sprite2, guy_sprite3]);
+  guy = new Animation(guy_sprite1, guy_sprite2, guy_sprite3);
 }
 
 function draw() {
-  
   background(255);
-  image(logo, windowWidth/2 - (logo.width/2), 10);
-  guy.animate(0.1, 10, windowHeight - guy_sprite1.height*0.15, guy_sprite1.width*0.15, guy_sprite1.height*0.15)
+  image(logo, windowWidth / 2 - logo.width / 2, 10);
+  guy.animate(
+    0.1,
+    10,
+    windowHeight - guy_sprite1.height * 0.15,
+    guy_sprite1.width * 0.15,
+    guy_sprite1.height * 0.15
+  );
   d.update();
-  console.log(d.velocity);
+  // console.log(d.velocity);
   d.show();
 }
 
@@ -41,37 +46,37 @@ function windowResized() {
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW){
-		up = 1;
-	}
-	if (keyCode === DOWN_ARROW){
-		down = 1;
-	}
-	if (keyCode === LEFT_ARROW){
-		left = 1;
-	}
-	if (keyCode === RIGHT_ARROW){
-		right = 1;
-	}
-	if (keyCode === SHIFT){
-		turbo = 3;
-	}
+  if (keyCode === UP_ARROW) {
+    up = 1;
+  }
+  if (keyCode === DOWN_ARROW) {
+    down = 1;
+  }
+  if (keyCode === LEFT_ARROW) {
+    left = 1;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    right = 1;
+  }
+  if (keyCode === SHIFT) {
+    turbo = 3;
+  }
 }
 
 function keyReleased() {
-	if (keyCode === UP_ARROW){
-		up = 0;
-	}
-	if (keyCode === DOWN_ARROW){
-		down = 0;
-	}
-	if (keyCode === LEFT_ARROW){
-		left = 0;
-	}
-	if (keyCode === RIGHT_ARROW){
-		right = 0;
-	}
-	if (keyCode === SHIFT){
-		turbo = 0;
-	}
+  if (keyCode === UP_ARROW) {
+    up = 0;
+  }
+  if (keyCode === DOWN_ARROW) {
+    down = 0;
+  }
+  if (keyCode === LEFT_ARROW) {
+    left = 0;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    right = 0;
+  }
+  if (keyCode === SHIFT) {
+    turbo = 0;
+  }
 }
